d1d14e956008285c77291e82e78cebf4b15f41ca
Anselm Schüler <mail@anselmschueler.com>
2022-08-05 21:04:26 +0200

    Fix systemd-boot.nix

--- systemd-boot.nix
+++ systemd-boot.nix
@@ -1,12 +1,10 @@
 {
-  boot = {
-    loader = {
-      systemd-boot = {
-        enable = true;
-        editor = false;
-      };
-      timeout = 0;
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      editor = false;
     };
+    timeout = 0;
     efi.canTouchEfiVariables = true;
   };
 }
