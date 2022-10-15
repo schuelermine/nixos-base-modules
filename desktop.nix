80576c21ab0ecd158a406d428cf1cb426dcf138f
Anselm Schüler <mail@anselmschueler.com>
2022-10-15 23:40:32 +0200

    Fixed

--- desktop.nix
+++ desktop.nix
@@ -1,6 +1,14 @@
 { themed-gnome-shell-overlay }:
 { pkgs, ... }: {
-  nixpkgs.overlays = [ themed-gnome-shell-overlay ];
+  nixpkgs.overlays = [
+    (themed-gnome-shell-overlay "${pkgs.yaru-theme}/share/gnome-shell/theme/Yaru/gnome-shell-theme.gresource")
+    (selfPkgs: superPkgs: {
+      yaru-theme = super.yaru-theme.overrideAttrs
+        (selfAttrs: superAttrs: {
+          mesonFlags = superAttrs.mesonFlags or [ ] ++ [ "-Dgnome-shell-gresource=TRUE" ];
+        });
+    })
+  ];
   services = {
     xserver = {
       enable = true;
--- flake.nix
+++ flake.nix
@@ -1,19 +1,22 @@
 {
   inputs.themed-gnome-shell-nix.url = "github:schuelermine/themed-gnome-shell-nix";
-  outputs = { self, themed-gnome-shell-nix }: {
-    nixosModules = {
-      default = import ./. { themed-gnome-shell-overlay = themed-gnome-shell-nix.overlays.default; };
-      console = import ./console.nix;
-      desktop = import ./desktop.nix { themed-gnome-shell-overlay = themed-gnome-shell-nix.overlays.default; };
-      fonts = import ./fonts.nix;
-      i18n = import ./i18n.nix;
-      kernel = import ./kernel.nix;
-      nix = import ./nix.nix;
-      pipewire = import ./pipewire.nix;
-      plymouth = import ./plymouth.nix;
-      shell = import ./shell.nix;
-      systemd-boot = import ./systemd-boot.nix;
-      systemd-target-fix = import ./systemd-target-fix.nix;
+  outputs = { self, themed-gnome-shell-nix }:
+    let args = { themed-gnome-shell-overlay = themed-gnome-shell-nix.overlays.default; };
+    in
+    {
+      nixosModules = {
+        default = import ./. args;
+        console = import ./console.nix;
+        desktop = import ./desktop.nix { inherit (args) themed-gnome-shell-overlay; };
+        fonts = import ./fonts.nix;
+        i18n = import ./i18n.nix;
+        kernel = import ./kernel.nix;
+        nix = import ./nix.nix;
+        pipewire = import ./pipewire.nix;
+        plymouth = import ./plymouth.nix;
+        shell = import ./shell.nix;
+        systemd-boot = import ./systemd-boot.nix;
+        systemd-target-fix = import ./systemd-target-fix.nix;
+      };
     };
-  };
 }
